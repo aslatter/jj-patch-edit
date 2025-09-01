5fd915a4b28de39a2c4693264763c91542b9019d
Antoine Latter <aslatter@gmail.com>
2025-09-01 08:57:00 -0500

    chunk up diff output

--- main.go
+++ main.go
@@ -23,7 +23,24 @@ func mainErr() error {
 
 	var iterErr error
 
+	var startedDiff bool
+	var currentPatch [][]byte
 	for line := range diffLines(os.Args[1], os.Args[2], &iterErr) {
+		if startedDiff {
+			if stoppingLine(line) {
+				// prompt for inclusion of everything before this line
+				fmt.Println("\nInclude change? ")
+				_, _ = fmt.Scanln()
+				fmt.Println()
+				// apply current patch
+				currentPatch = nil
+				startedDiff = false
+			}
+		}
+		if isStartOfDiff(line) {
+			startedDiff = true
+		}
+		currentPatch = append(currentPatch, line)
 		fmt.Println(string(line))
 	}
 	if iterErr != nil {
@@ -35,6 +52,26 @@ func mainErr() error {
 
 	// until we know what we're doing, exit with failure
 	return errors.New("unimplemented")
+}
+
+func isStartOfDiff(line []byte) bool {
+	if len(line) == 0 {
+		return false
+	}
+	return line[0] == '@'
+}
+
+func stoppingLine(line []byte) bool {
+	if len(line) == 0 {
+		// ??
+		return false
+	}
+	c := line[0]
+	switch c {
+	case '-', '+', ' ':
+		return false
+	}
+	return true
 }
 
 func diffLines(leftPath string, rightPath string, outErr *error) iter.Seq[[]byte] {
